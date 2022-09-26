6e0292e352a09c0df2394650104784d12ccbcab1
MasonGarrod <113171947+MasonGarrod@users.noreply.github.com>
2022-09-26 10:58:05 +0100

    Add files via upload

---
+++ DiceRoll/DiceRoll/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+
+            int diceRoll = 0;
+            int attempts = 0;
+
+            Console.WriteLine("Press any key to roll the dice");
+
+            while (diceRoll != 6)
+            {
+                Console.ReadKey();
+                diceRoll = rng.Next(1, 7);
+                Console.WriteLine("You rolled a " + diceRoll);
+                attempts++;
+            }
+
+            Console.WriteLine("You have attempted " + attempts + " attempts to roll a 6");
+
+            Console.ReadKey();
+        }
+    }
+}
